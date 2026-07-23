b08c89e64edd9384ed9c2839ceff11eafa06cd08
Ahmed Mahmoud <Ahmedgf363@gmail.com>
2026-07-23 19:25:17 +0300

    Edited Web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthenticatedSessionController;
-use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\AuthenticatedSessionController;
 
-Route::get("/register", [RegisteredUserController::class,'create'] )->name('register');
-Route::post('/register', [RegisteredUserController::class,'store'])->name('register.store');
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
 
-Route::get('/login', [AuthenticatedSessionController::class,'create'])->name('login');
-Route::post('/login', [AuthenticatedSessionController::class,'store'])->name('login.store');
+Route::middleware('guest')->group(function () {
 
-Route::post('/logout', [AuthenticatedSessionController::class,'destroy'])->name('logout');
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->name('register.store');
+
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->name('login.store');
+
+});
+
+Route::middleware('auth')->group(function () {
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+
+});
