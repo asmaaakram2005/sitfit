78efe6dc6a3a21482137126bb4d2646cad72dc07
Ahmed Mahmoud <Ahmedgf363@gmail.com>
2026-07-25 15:42:49 +0300

    added cart.blade.php and cart.css files

---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+
+@section('css')
+
+<link rel="stylesheet" href="{{ asset('css/cart.css') }}">
+
+@endsection
+
+
+
+@section('title')
+
+<!-- write The title here like 'Home page' with out anything just string. "Ahmed" -->
+
+@endsection
+
+
+
+
+@section('content')
+
+<!-- Write all codes of page here without write <html> or <body>. "Ahmed" -->
+
+@endsection
