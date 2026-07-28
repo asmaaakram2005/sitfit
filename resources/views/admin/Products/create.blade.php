4f20b7c18643f9f75fb2f4ce5c9ce522047d9839
Ahmed Mahmoud <Ahmedgf363@gmail.com>
2026-07-28 22:07:04 +0300

    Fiexed layouts admin in all files

---
+++ resources/views/admin/Products/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('title', 'page title')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin/products.css') }}">
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>Hello Ms.Sondos</h1>
+    </div>
+@endsection
