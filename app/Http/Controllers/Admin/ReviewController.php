c0a462cd42ef31bc0b5067ce9c9313a9302dd235
sondos <sondoshitham66@gmail.com>
2026-07-31 14:00:25 +0300

    update

--- app/Http/Controllers/Admin/ReviewController.php
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Review;
+
 
 class ReviewController extends Controller
 {
     public function index()
     {
-        return view('admin.reviews.index');
+         $reviews = Review::with(['user', 'product'])->get();
+        return view('admin.reviews.index',[
+            'reviews' => $reviews
+        ]);
     }
 }
